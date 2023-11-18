dd0c5b02ef20426d6d92a5de18f266ba9d860ae8
Naveen <naveensingh0630@gmail.com>
2023-11-18 17:52:52 +0530

    Improved v2

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -31,14 +31,15 @@ const Hero = () => {
                     href="/dist/NSGPOS Setup 0.2.2.exe"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                     target="_blank"
-                    download
+                    rel="noreferrer"
                   >
-                    🔥 Download Software
+                    🔥 Download Desktop App 
                   </a>
                   <a
-                    href="https://github.com/NSG1112/nsgpos"
+                    href="https://github.com/NSG1112/nsg-pos-appe-electron"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                     target="_blank"
+                    rel="noreferrer"
                   >
                     Contribute on Github
                   </a>
